220f06d629d8ea0eafb48da7b82840a18c29d173
eld11ar <eldiiar.saparbekov.dev@gmail.com>
2025-07-21 20:49:56 +0600

    chore: lint-staged

--- src/pages/SignIn/ui/WavesSvg.tsx
+++ src/pages/SignIn/ui/WavesSvg.tsx
@@ -1,6 +1,6 @@
 export const WavesSvg = () => {
 	return (
-		// biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+		// biome-ignore lint/a11y/noSvgWithoutTitle: not needed
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			viewBox="0 0 1600 800"
