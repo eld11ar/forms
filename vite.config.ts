f17bf4c6aa28102bf6ea88331270583d06e26694
eld11ar <eldiiar.saparbekov.dev@gmail.com>
2025-07-24 22:50:50 +0600

    chore: bundle visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import path from "node:path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import { visualizer } from "rollup-plugin-visualizer"
+import { defineConfig, type PluginOption } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [react(), tailwindcss(), visualizer({ open: true }) as PluginOption],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
